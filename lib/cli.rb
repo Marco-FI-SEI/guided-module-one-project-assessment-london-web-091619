253d272b5df02ef5710647557212e89a88bfa867
Marco-FI-SEI <mmc90@hotmail.co.uk>
2019-10-03 17:51:58 +0100

    Completed MVP

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,197 +1,244 @@
+require "pry"
 require "tty-prompt"
-require 'encrypted_strings'
+require "encrypted_strings"
 
 class CLI
-  
   @@prompt = TTY::Prompt.new
 
-  
+  #--------------------------------------------------------------------------------------------------------#
+  # RUN METHOD #
+  #--------------------------------------------------------------------------------------------------------#
 
-    def run
-      choice = @@prompt.select("Sign up or Log in to enjoy our fantastic Gelato!", "Log In", "Sign Up")
-      if choice == "Sign Up"
-        @current_user = sign_up_user
-      else
-        @current_user = log_in_user
-      end
+  def run
+    choice = @@prompt.select("Sign up or Log in to enjoy our fantastic Gelato!", "Log In", "Sign Up")
+    choice == "Sign Up" ? @current_user = sign_up_user : @current_user = log_in_user
 
-      if @current_user
-        main_menu
-      else
-        puts "Sorry, your username/password did not match. Please try again."
-        run
-      end
+    if @current_user
+      main_menu
+    else
+      puts "Sorry, your username/password did not match. Please try again."
+      run
     end
+  end
 
-    def sign_up_user
-      # 
-      
-      fullName = @@prompt.ask("Please enter your Full Name: ")
-      emailAdd = @@prompt.ask("Please enter your Email Address (This will be your Username): ")
-      postalAdd = @@prompt.ask("Please enter your Postal Address: ")
-      password1 = @@prompt.mask("Please enter your Password: ")
-      password2 = @@prompt.mask("For security purposes, Please Re-enter your Password: ")
-      if password1 == password2
-        User.create(name: fullName, password: password1.encrypt, address: postalAdd, email: emailAdd)
-      end
+  #--------------------------------------------------------------------------------------------------------#
+  # USER ACCOUNT #
+  #--------------------------------------------------------------------------------------------------------#
+
+  def sign_up_user
+    full_name = @@prompt.ask("Please enter your Full Name: ")
+    email_add = @@prompt.ask("Please enter your Email Address (This will be your Username): ")
+    postal_add = @@prompt.ask("Please enter your Postal Address: ")
+    password1 = @@prompt.mask("Please enter your Password: ")
+    password2 = @@prompt.mask("For security purposes, Please Re-enter your Password: ")
+    if password1 == password2
+      User.create(name: full_name, password: password1.encrypt, address: postal_add, email: email_add)
     end
+  end
 
+  def log_in_user
+    username = @@prompt.ask("Please enter your username: ")
+    password = @@prompt.mask("Please enter your password: ")
+    User.find_by(email: username, password: password.encrypt)
+  end
 
-    def log_in_user
-      # 
+  def good_bye
+    system("clear")
+    puts "Thank you for visiting K&M Gelato! Come back soon!"
+  end
 
-      username = @@prompt.ask("Please enter your username: ")
-      password = @@prompt.mask("Please enter your password: ")
-      User.find_by(address: username, password: password.encrypt)
+  #--------------------------------------------------------------------------------------------------------#
+  # MAIN MENU #
+  #--------------------------------------------------------------------------------------------------------#
+
+  def main_menu
+    sleep(1)
+    system("clear")
+    menu_choice = @@prompt.select("Hello there! What can we do for you today?",
+                                  "Create New Order", "Edit Order", "Cancel Order", "My Orders", "About/Promo", "Exit")
+
+    case menu_choice
+    when "Create New Order"
+      create_order
+      main_menu
+    when "Edit Order"
+      edit_order
+      where_to_next
+    when "Cancel Order"
+      cancel_order
+      where_to_next
+    when "My Orders"
+      display_orders(my_orders)
+      where_to_next
+    when "About/Promo"
+      about_us
+      where_to_next
+    when "Exit"
+      good_bye
+    else
+      puts "You seem to have broken our application, thank you!"
     end
+  end
 
-    def main_menu
-      system('clear')
-      # 
-      puts "Hello there!"
-      menu_choice = @@prompt.select("What can we do for you today?", "Create New Order", "Edit Order", "Cancel Order", "My Orders", "About/Promo")
-    
-      case menu_choice
-      when "Create New Order"
-        create_order
-        puts "Thanks, we're on it!"
-        main_menu
-      when "Edit Order"
-        # edit method
-        edit_order
-        puts "Your order has been edited"
-        where_to_next
-      when "Cancel Order"
-        # delete method
-        cancel_order
-        puts "Your order has been cancelled"
-        where_to_next
-      when "My Orders" 
-        puts "These are your orders!"
-        my_orders
-        where_to_next
-      when "About/Promo"
-        #post about us
-        about_us
-        where_to_next
-      else
-        puts "you seem to have broken our application, thank you!"
-      end
-    end
+  #--------------------------------------------------------------------------------------------------------#
+  # ORDERS #
+  #--------------------------------------------------------------------------------------------------------#
 
-    def create_order
-      # 
-      servings = @@prompt.ask("How many servings would you like?: ")
-      validate_input("from create", servings.to_i)
+  def create_order
+    servings = @@prompt.ask("How many servings would you like? ")
+    if servings_valid?(servings.to_i)
       total = servings.to_f * 5.0
-      remove_stock(servings)
-      Order.create(user_id: @current_user.id, gelato_id: current_gelato.id, order_time: order_timestamp , status: "pending", total: total, servings: servings)
-      stock_control(servings)
+      Order.create(user_id: @current_user.id, gelato_id: get_gelato(1).id, order_time: order_timestamp,
+                   status: "pending", total: total, servings: servings)
+      remove_stock(servings, get_gelato(1).id)
+      stock_control(servings, get_gelato(1).id)
+
+      puts "Thanks, we're on it!"
+    else
+      create_order
     end
+  end
 
-    def edit_order
-      order_to_edit = @@prompt.select("Which order would you like to edit?", pending_orders)
-      servings = @@prompt.ask("How many servings would you like instead?: ")
-      # control max and min order amounts
-      validate_input("from edit", servings.to_i)
-      # add or remove from stock
-      add_stock(order_to_edit.servings)
-      remove_stock(servings)
-      # update total
-      update_total(order_to_edit, servings)
-      # request more stock
-      stock_control(servings)
-      # update order information
-      order_to_edit.servings = servings
-      order_to_edit.save
+  def string_order_to_object(string)
+    id = string.partition(".").first
+    order = Order.find(id)
+  end
+
+  def go_to_menu(string)
+    main_menu if string == "Main menu"
+  end
+
+  def edit_order
+    order_to_edit = @@prompt.select("Which order would you like to edit?", orders_selection, "Main menu")
+    go_to_menu(order_to_edit)
+    order_object = string_order_to_object(order_to_edit)
+    servings = @@prompt.ask("How many servings would you like instead?")
+    if servings_valid?(servings.to_i)
+      add_stock(order_object.servings, order_object.gelato_id)
+      remove_stock(servings, order_object.gelato_id)
+      update_total(order_object, servings)
+      stock_control(servings, get_gelato(1).id)
+      order_object.servings = servings
+      order_object.save
+      puts "Your order has been edited"
+    else
+      edit_order
     end
+  end
 
-    def cancel_order
-      # 
-      cancelled_order = @@prompt.select("Which order would you like to cancel?", pending_orders) 
-      cancelled_order.status = "cancelled"
-      cancelled_order.save
-      add_stock(cancelled_order.servings)
+  def cancel_order
+    order_to_cancel = @@prompt.select("Which order would you like to cancel?", orders_selection, "Main menu")
+    go_to_menu(order_to_cancel)
+    order_object = string_order_to_object(order_to_cancel)
+    order_object.save
+    add_stock(order_object.servings, get_gelato(1).id)
+    puts "Your order has been cancelled"
+    delete_order(order_object)
+  end
+
+  def delete_order(order)
+    Order.delete(order.id)
+  end
+
+  def display_orders(orders)
+    puts "\nYour orders:\n"
+    orders.each_with_index do |order, index|
+      puts "#{index + 1}: #{order.servings} servings of #{get_gelato(order.gelato_id).flavour} at #{order.order_time}. Order total was: £#{order.total}"
     end
+    puts "\n"
+  end
 
-    def my_orders
-      # 
-      puts "Here are your current orders"
-      user_orders = Order.all.select {|order| order.user_id == @current_user.id }
-      p user_orders
+  def orders_selection
+    pending_orders.map { |order|
+      "     #{order.id}.
+       Date and Time: #{order.order_time.to_s}
+       Flavour: #{get_gelato(order.gelato_id).flavour}
+       Servings: #{order.servings.to_s}"
+    }
+  end
+
+  def my_orders
+    Order.all.select { |order| order.user_id == @current_user.id }
+  end
+
+  def pending_orders
+    my_orders.select { |order| order.status == "pending" }
+  end
+
+  def order_timestamp
+    Time.now.strftime("%d/%m/%Y, %H:%M")
+  end
+
+  def update_total(order_to_edit, new_servings)
+    order_to_edit.total = new_servings.to_f * 5
+  end
+
+  #--------------------------------------------------------------------------------------------------------#
+  # STOCK #
+  #--------------------------------------------------------------------------------------------------------#
+
+  def remove_stock(servings, id)
+    current_gelato = get_gelato(id)
+    current_gelato.stock -= servings.to_f
+    current_gelato.save
+  end
+
+  def add_stock(order_stock, id)
+    current_gelato = get_gelato(id)
+    current_gelato.stock += order_stock.to_f
+    current_gelato.save
+  end
+
+  def stock_control(servings, id)
+    current_stock = get_gelato(id).stock
+    add_stock(100 - current_stock, id) if current_stock <= servings.to_i || current_stock <= 10
+  end
+
+  def get_gelato(id)
+    Gelato.find(id)
+  end
+
+  #--------------------------------------------------------------------------------------------------------#
+  # VALIDATION #
+  #--------------------------------------------------------------------------------------------------------#
+  # validates user input and calls create or edit order depending on flag passed in
+
+  def servings_valid?(servings)
+    if servings > 50
+      system("clear")
+      puts "Due to high demand, we can only complete orders of up to 50 servings at one time. Sorry for the inconvenience.\n"
+      return false
+    elsif servings <= 0
+      system("clear")
+      puts "Please enter a valid number of servings.\n"
+      return false
     end
+    true
+  end
 
-    def about_us
-      puts "Here at K&M, we pride ourselves on our ability to provide you with the highest \nquality Gelato at the very best price. Our unique boutique blend of rich and chewy \nchocolate brownies combined with decadent caramel swirls will leave you begging to \nknow the recipe. But wait, there's more! Sign up and order with us today or spend £20\nor more in store to recieve a 10% discount off your total order cost. \n\nSo what are you waiting for? Order now!"
-    end  
+  #--------------------------------------------------------------------------------------------------------#
+  # PROGRAM FLOW #
+  #--------------------------------------------------------------------------------------------------------#
 
-    def remove_stock(servings)
-      # 
-      my_gelato = current_gelato
-      my_gelato.stock -= servings.to_f
-      my_gelato.save
-    end
+  # call create or edit method based on flag
+  def enact_flag(flag)
+    flag == "from create" ? create_order : edit_order
+  end
 
-    def add_stock(order_stock)
-      # 
-      my_gelato = current_gelato 
-      my_gelato.stock += order_stock.to_f
-      my_gelato.save
-    end
+  # called after user performs action to get next step
+  def where_to_next
+    response = @@prompt.select("Is there anything else we can do for you today?\n", "Yes, please!",
+                               "No, thank you!")
+    response == "Yes, please!" ? main_menu : good_bye
+  end
 
-    def current_gelato
-      # 
-      Gelato.find(1)
-    end
+  #--------------------------------------------------------------------------------------------------------#
+  # About #
+  #--------------------------------------------------------------------------------------------------------#
 
-    def pending_orders
-      # 
-      my_orders.select{|order| order.status == "pending"}
-    end
-
-    def stock_control(servings)
-      add_stock(100 - current_gelato.stock) if current_gelato.stock <= servings.to_i || current_gelato.stock <= 10
-    end
-
-    def validate_input(location, servings)
-      if servings > 50
-        puts "Due to high demand, we can only complete orders of up to 50 servings at one time. Sorry for the inconvenience."
-        find_location(location)
-      elsif servings <= 0
-        answer = @@prompt.select("This will delete your order. Are you sure you wish to proceed?", "Yes", "No")
-        if answer == "Yes"
-          cancel_order
-        else
-          find_location(location)
-        end
-      end
-    end
-
-    def find_location(location)
-      location == "from create" ? create_order : edit_order
-    end
-
-    def update_total(order_to_edit, new_servings)
-      order_to_edit.total = new_servings.to_f * 5
-    end
-
-    def order_timestamp
-      # 
-      Time.now.to_s.split(" ")[0..1].join(" ")
-    end
-
-    def where_to_next
-      response = @@prompt.select("Is there anything else we can do for you today?", "Yes, please!", "No, thank you!")
-      response == "Yes, please!" ? main_menu : good_bye
-    end
-
-    def good_bye
-      system('clear')
-      puts "Thank you for visiting K&M Gelato! Come back soon!"
-    end
-
-
-
+  def about_us
+    puts "\n\nHere at K&M, we pride ourselves on our ability to provide you with the highest \nquality Gelato at the very best price. Our unique boutique blend of rich and chewy \nchocolate brownies combined with decadent caramel swirls will leave you begging to \nknow the recipe. But wait, there's more! Sign up and order with us today or spend £20\nor more in store to recieve a 10% discount off your total order cost. \n\nSo what are you waiting for? Order now!\n\n"
+  end
 end
 
 # ADMIN USER
--- lib/gelato.rb
+++ lib/gelato.rb
@@ -1,8 +1,20 @@
+require "pry"
+
 class Gelato < ActiveRecord::Base
   has_many :orders
   has_many :users, through: :orders
 
   def current_stock
     Gelato.all.first.stock
+  end
+
+  def self.get_flavours
+    # Gelato.all.map(&:flavour)
+    Gelato.all
+  end
+
+  def get_gelato_flavour_from_order(order)
+    gelato_id = order.gelato_id
+    # get_flavours.select{ |flavour| flavour == }
   end
 end
